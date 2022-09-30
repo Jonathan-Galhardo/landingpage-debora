c1ba017ab9dd95671ce0a0d3c999b6c5fc44ddc4
Jonathan-Galhardo <galhardoj@gmail.com>
2022-09-30 16:37:14 -0300

    Seção Nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './components/header/Header'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Nav from './components/Nav/Nav'
 
 const App = () => {
   return (
     <>
       <Header />
+      <Nav />
       < About />
       <Contact />
     </>
